7ad646d7eafc213f0e4ed06b117ec837aac98a8d
Jitae9605 <dlwlxo3819@naver.com>
2022-05-03 12:03:46 +0900

    초기 커밋.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace Lecture_Csharp_Winform_2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.IbIFont = new System.Windows.Forms.Label();
+			this.grpFont = new System.Windows.Forms.GroupBox();
+			this.txtSampleText = new System.Windows.Forms.TextBox();
+			this.cboFont = new System.Windows.Forms.ComboBox();
+			this.chkItalic = new System.Windows.Forms.CheckBox();
+			this.chkBold = new System.Windows.Forms.CheckBox();
+			this.grpBar = new System.Windows.Forms.GroupBox();
+			this.pgDummy = new System.Windows.Forms.ProgressBar();
+			this.tbDummy = new System.Windows.Forms.TrackBar();
+			this.btnModal = new System.Windows.Forms.Button();
+			this.btnModaless = new System.Windows.Forms.Button();
+			this.btnMsgBox = new System.Windows.Forms.Button();
+			this.grpTreeList = new System.Windows.Forms.GroupBox();
+			this.lvDummy = new System.Windows.Forms.ListView();
+			this.tvDummy = new System.Windows.Forms.TreeView();
+			this.btnAddChild = new System.Windows.Forms.Button();
+			this.btnAddRoot = new System.Windows.Forms.Button();
+			this.grpFont.SuspendLayout();
+			this.grpBar.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.tbDummy)).BeginInit();
+			this.grpTreeList.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// IbIFont
+			// 
+			this.IbIFont.AutoSize = true;
+			this.IbIFont.Location = new System.Drawing.Point(40, 58);
+			this.IbIFont.Name = "IbIFont";
+			this.IbIFont.Size = new System.Drawing.Size(41, 12);
+			this.IbIFont.TabIndex = 0;
+			this.IbIFont.Text = "Font : ";
+			// 
+			// grpFont
+			// 
+			this.grpFont.Controls.Add(this.txtSampleText);
+			this.grpFont.Controls.Add(this.cboFont);
+			this.grpFont.Controls.Add(this.chkItalic);
+			this.grpFont.Controls.Add(this.chkBold);
+			this.grpFont.Controls.Add(this.IbIFont);
+			this.grpFont.Location = new System.Drawing.Point(27, 12);
+			this.grpFont.Name = "grpFont";
+			this.grpFont.Size = new System.Drawing.Size(441, 175);
+			this.grpFont.TabIndex = 1;
+			this.grpFont.TabStop = false;
+			this.grpFont.Text = "CombBoX, CheckBox, TextBox";
+			// 
+			// txtSampleText
+			// 
+			this.txtSampleText.Location = new System.Drawing.Point(42, 96);
+			this.txtSampleText.Name = "txtSampleText";
+			this.txtSampleText.Size = new System.Drawing.Size(301, 21);
+			this.txtSampleText.TabIndex = 2;
+			this.txtSampleText.Text = "Hello, C#";
+			// 
+			// cboFont
+			// 
+			this.cboFont.FormattingEnabled = true;
+			this.cboFont.Location = new System.Drawing.Point(87, 55);
+			this.cboFont.Name = "cboFont";
+			this.cboFont.Size = new System.Drawing.Size(121, 20);
+			this.cboFont.TabIndex = 2;
+			this.cboFont.SelectedIndexChanged += new System.EventHandler(this.cboFont_SelectedIndexChanged);
+			// 
+			// chkItalic
+			// 
+			this.chkItalic.AutoSize = true;
+			this.chkItalic.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.chkItalic.Location = new System.Drawing.Point(283, 57);
+			this.chkItalic.Name = "chkItalic";
+			this.chkItalic.Size = new System.Drawing.Size(60, 16);
+			this.chkItalic.TabIndex = 1;
+			this.chkItalic.Text = "이탤릭";
+			this.chkItalic.UseVisualStyleBackColor = true;
+			this.chkItalic.CheckedChanged += new System.EventHandler(this.chkItalic_CheckedChanged);
+			// 
+			// chkBold
+			// 
+			this.chkBold.AutoSize = true;
+			this.chkBold.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.chkBold.Location = new System.Drawing.Point(227, 57);
+			this.chkBold.Name = "chkBold";
+			this.chkBold.Size = new System.Drawing.Size(50, 16);
+			this.chkBold.TabIndex = 1;
+			this.chkBold.Text = "굵게";
+			this.chkBold.UseVisualStyleBackColor = true;
+			this.chkBold.CheckedChanged += new System.EventHandler(this.chkBold_CheckedChanged);
+			// 
+			// grpBar
+			// 
+			this.grpBar.Controls.Add(this.pgDummy);
+			this.grpBar.Controls.Add(this.tbDummy);
+			this.grpBar.Location = new System.Drawing.Point(27, 193);
+			this.grpBar.Name = "grpBar";
+			this.grpBar.Size = new System.Drawing.Size(441, 150);
+			this.grpBar.TabIndex = 2;
+			this.grpBar.TabStop = false;
+			this.grpBar.Text = "TrackBar && ProgressBar";
+			// 
+			// pgDummy
+			// 
+			this.pgDummy.Location = new System.Drawing.Point(15, 93);
+			this.pgDummy.Maximum = 20;
+			this.pgDummy.Name = "pgDummy";
+			this.pgDummy.Size = new System.Drawing.Size(406, 24);
+			this.pgDummy.TabIndex = 1;
+			// 
+			// tbDummy
+			// 
+			this.tbDummy.Location = new System.Drawing.Point(15, 32);
+			this.tbDummy.Maximum = 20;
+			this.tbDummy.Name = "tbDummy";
+			this.tbDummy.Size = new System.Drawing.Size(406, 45);
+			this.tbDummy.TabIndex = 0;
+			this.tbDummy.Scroll += new System.EventHandler(this.tbDummy_Scroll);
+			// 
+			// btnModal
+			// 
+			this.btnModal.Location = new System.Drawing.Point(63, 349);
+			this.btnModal.Name = "btnModal";
+			this.btnModal.Size = new System.Drawing.Size(96, 39);
+			this.btnModal.TabIndex = 3;
+			this.btnModal.Text = "Modal";
+			this.btnModal.UseVisualStyleBackColor = true;
+			this.btnModal.Click += new System.EventHandler(this.btnModal_Click);
+			// 
+			// btnModaless
+			// 
+			this.btnModaless.Location = new System.Drawing.Point(182, 349);
+			this.btnModaless.Name = "btnModaless";
+			this.btnModaless.Size = new System.Drawing.Size(96, 39);
+			this.btnModaless.TabIndex = 3;
+			this.btnModaless.Text = "Modaless";
+			this.btnModaless.UseVisualStyleBackColor = true;
+			this.btnModaless.Click += new System.EventHandler(this.btnModaless_Click);
+			// 
+			// btnMsgBox
+			// 
+			this.btnMsgBox.Location = new System.Drawing.Point(304, 349);
+			this.btnMsgBox.Name = "btnMsgBox";
+			this.btnMsgBox.Size = new System.Drawing.Size(96, 39);
+			this.btnMsgBox.TabIndex = 3;
+			this.btnMsgBox.Text = "MessageBox";
+			this.btnMsgBox.UseVisualStyleBackColor = true;
+			this.btnMsgBox.Click += new System.EventHandler(this.btnMsgBox_Click);
+			// 
+			// grpTreeList
+			// 
+			this.grpTreeList.Controls.Add(this.lvDummy);
+			this.grpTreeList.Controls.Add(this.tvDummy);
+			this.grpTreeList.Controls.Add(this.btnAddChild);
+			this.grpTreeList.Controls.Add(this.btnAddRoot);
+			this.grpTreeList.Location = new System.Drawing.Point(27, 394);
+			this.grpTreeList.Name = "grpTreeList";
+			this.grpTreeList.Size = new System.Drawing.Size(441, 227);
+			this.grpTreeList.TabIndex = 4;
+			this.grpTreeList.TabStop = false;
+			this.grpTreeList.Text = "TreeView && ListView";
+			// 
+			// lvDummy
+			// 
+			this.lvDummy.HideSelection = false;
+			this.lvDummy.Location = new System.Drawing.Point(227, 20);
+			this.lvDummy.Name = "lvDummy";
+			this.lvDummy.Size = new System.Drawing.Size(194, 192);
+			this.lvDummy.TabIndex = 5;
+			this.lvDummy.UseCompatibleStateImageBehavior = false;
+			this.lvDummy.View = System.Windows.Forms.View.Details;
+			// 
+			// tvDummy
+			// 
+			this.tvDummy.Location = new System.Drawing.Point(15, 20);
+			this.tvDummy.Name = "tvDummy";
+			this.tvDummy.Size = new System.Drawing.Size(206, 163);
+			this.tvDummy.TabIndex = 6;
+			// 
+			// btnAddChild
+			// 
+			this.btnAddChild.Location = new System.Drawing.Point(133, 189);
+			this.btnAddChild.Name = "btnAddChild";
+			this.btnAddChild.Size = new System.Drawing.Size(75, 23);
+			this.btnAddChild.TabIndex = 5;
+			this.btnAddChild.Text = "자식추가";
+			this.btnAddChild.UseVisualStyleBackColor = true;
+			this.btnAddChild.Click += new System.EventHandler(this.btnAddChild_Click);
+			// 
+			// btnAddRoot
+			// 
+			this.btnAddRoot.Location = new System.Drawing.Point(36, 189);
+			this.btnAddRoot.Name = "btnAddRoot";
+			this.btnAddRoot.Size = new System.Drawing.Size(75, 23);
+			this.btnAddRoot.TabIndex = 5;
+			this.btnAddRoot.Text = "루트추가";
+			this.btnAddRoot.UseVisualStyleBackColor = true;
+			this.btnAddRoot.Click += new System.EventHandler(this.btnAddRoot_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(497, 644);
+			this.Controls.Add(this.grpTreeList);
+			this.Controls.Add(this.btnMsgBox);
+			this.Controls.Add(this.btnModaless);
+			this.Controls.Add(this.btnModal);
+			this.Controls.Add(this.grpBar);
+			this.Controls.Add(this.grpFont);
+			this.Name = "MainForm";
+			this.Text = "Control Test";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.grpFont.ResumeLayout(false);
+			this.grpFont.PerformLayout();
+			this.grpBar.ResumeLayout(false);
+			this.grpBar.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.tbDummy)).EndInit();
+			this.grpTreeList.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label IbIFont;
+		private System.Windows.Forms.GroupBox grpFont;
+		private System.Windows.Forms.CheckBox chkItalic;
+		private System.Windows.Forms.CheckBox chkBold;
+		private System.Windows.Forms.TextBox txtSampleText;
+		private System.Windows.Forms.ComboBox cboFont;
+		private System.Windows.Forms.GroupBox grpBar;
+		private System.Windows.Forms.ProgressBar pgDummy;
+		private System.Windows.Forms.TrackBar tbDummy;
+		private System.Windows.Forms.Button btnModal;
+		private System.Windows.Forms.Button btnModaless;
+		private System.Windows.Forms.Button btnMsgBox;
+		private System.Windows.Forms.GroupBox grpTreeList;
+		private System.Windows.Forms.ListView lvDummy;
+		private System.Windows.Forms.TreeView tvDummy;
+		private System.Windows.Forms.Button btnAddChild;
+		private System.Windows.Forms.Button btnAddRoot;
+	}
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture_Csharp_Winform_2
+{
+	public partial class MainForm : Form
+	{
+		Random random = new Random(37);
+		public MainForm()
+		{
+			InitializeComponent();
+
+			// lvDummy에 칼럼을 생성하는 코드 
+			lvDummy.Columns.Add("Name");
+			lvDummy.Columns.Add("Depth");
+		}
+
+		void ChangeFont()
+		{
+			if (cboFont.SelectedIndex < 0)      // cboFont에서 선택한 항목이 없으면 메소드 종료
+				return;
+
+			FontStyle style = FontStyle.Regular;        // FontStyle 객체 초기화
+
+
+			// 굵게와 이탤릭은 동시 적용이 가능하므로 논리합으로 진행해야한다.
+			if (chkBold.Checked)    // 굵게 체크박스가 선택되어있으면 Bold논리합 수행
+				style |= FontStyle.Bold;
+
+			if (chkItalic.Checked)    //	이탤릭 체크박스가 선택되어있으면 Italic논리합 수행
+				style |= FontStyle.Italic;
+
+
+			txtSampleText.Font =    // txtSampleText의 Font프로퍼티를 앞에서 만든 style로 수정
+				new Font((string)cboFont.SelectedItem, 10, style);
+		}
+
+		private void cboFont_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			// 컨트롤박스에서 선택된게 변화가 생기면 실행
+			ChangeFont();
+		}
+
+		private void chkItalic_CheckedChanged(object sender, EventArgs e)
+		{
+			// 체크박스 체크상태에 변화생기면 실행
+			ChangeFont();
+		}
+
+		private void chkBold_CheckedChanged(object sender, EventArgs e)
+		{
+			// 체크박스 체크상태에 변화생기면 실행
+			ChangeFont();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			var Fonts = FontFamily.Families;            // 운영체제에 설치되있는 폰트목록검색
+
+			foreach (FontFamily font in Fonts)          // cboFont 컨트롤에 각 폰트 이름 추가
+				cboFont.Items.Add(font.Name);
+		}
+
+		private void tbDummy_Scroll(object sender, EventArgs e)
+		{
+			// 트랙바를 슬라이드할때 실행
+			pgDummy.Value = tbDummy.Value;	// 슬라이더의 위치에 따라 프로그레스바의 내용도 변경
+		}
+
+		private void btnModal_Click(object sender, EventArgs e)
+		{
+			Form frm = new Form();
+			frm.Text = "Modal Form";
+			frm.Width = 300;
+			frm.Height = 100;
+			frm.BackColor = Color.Red;
+			frm.ShowDialog();				// Modal창을 띄웁니다.
+		}
+
+		private void btnModaless_Click(object sender, EventArgs e)
+		{
+			Form frm = new Form();
+			frm.Text = "Modaless Form";
+			frm.Width = 300;
+			frm.Height = 300;
+			frm.BackColor = Color.Green;
+			frm.Show();						// Modaless창을 띄웁니다.
+		}
+
+		private void btnMsgBox_Click(object sender, EventArgs e)
+		{
+			// MessageBox를 띄웁니다.
+			MessageBox.Show(txtSampleText.Text, "MessageBox Test", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+			//					메세지박스내용		타이틀			메세지박스 버튼추가		메세지박스 아이콘 추가
+		}
+
+		void TreeToList()
+		{
+			// TreeView의 각 노드를 ListView로 옮겨 표시하는 기능
+			lvDummy.Items.Clear();						// 리스트뷰 내용 초기화
+			foreach (TreeNode node in tvDummy.Nodes)    // 트리뷰의 각각의 루트노드들에 TreeToList(node) 수행
+				TreeToList(node);
+		}
+
+		void TreeToList(TreeNode Node)
+		{
+			// TreeView의 각 노드를 ListView로 옮겨 표시
+			lvDummy.Items.Add(
+				new ListViewItem(
+					new string[] { Node.Text, Node.FullPath.Count(f => f == '\\').ToString() }
+					)	//			Name			Depth			
+				);
+			// TreeNode형식의 FullPath 프로퍼티는 루트노드부터 현재노드까지의 경로를 나타낸다.
+			// 이때 각 경로는 '\\'로 구분된다. 즉, 이 갯수를 세는 것으로 깊이(Depth)를 카운팅
+			// 루트 = 0
+
+			foreach(TreeNode node in Node.Nodes)
+			{
+				// 현재의 노드의 자식노드들에 대해서도 같은 일을 반복
+				TreeToList(node);
+			}
+		}
+
+		private void btnAddRoot_Click(object sender, EventArgs e)
+		{
+			tvDummy.Nodes.Add(random.Next().ToString());
+			TreeToList();
+		}
+
+		private void btnAddChild_Click(object sender, EventArgs e)
+		{
+			if(tvDummy.SelectedNode == null)
+			{
+				MessageBox.Show("선택된 노드가 없습니다.", "TreeView Test", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			tvDummy.SelectedNode.Nodes.Add(random.Next().ToString());
+			tvDummy.SelectedNode.Expand();
+			TreeToList();
+
+		}
+	}
+}
